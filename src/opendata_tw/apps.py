ed9502a2feb8883f280357e98cfa4ecc7f1e7020
yellowsoar <yellowsoar@gmail.com>
2024-04-03 12:13:14 +0800

    add: init opendata_tw Django app

---
+++ src/opendata_tw/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OpendataTwConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "opendata_tw"
---
+++ src/opendata_tw/models.py
@@ -0,0 +1,5 @@
+from django.contrib.gis.db import models as geo_models
+from django.db import models
+
+# Create your models here.
+
