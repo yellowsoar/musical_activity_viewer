f4ddd756ae3d53bcce7cef8891c0b80ce56e82ca
yellowsoar <yellowsoar@gmail.com>
2024-04-05 01:57:02 +0800

    add: api remap table
    Why:
    remap the keys from the source data to more accurate names

--- src/opendata_tw/utils/update_data.py
+++ src/opendata_tw/utils/update_data.py
@@ -9,3 +9,27 @@ def request_data():
     )
     data_json = json.loads(data_raw.text)
     return data_json
+
+
+api_remap_table = {
+    'UID': 'id',
+    'comment': 'additional_remark',
+    'category': 'category_code',
+    'hitRate': 'click_through_rate',
+    'descriptionFilterHtml': 'description_content',
+    'discountInfo': 'discount_info',
+    'sourceWebName': 'flyer_source',
+    'version': 'publish_version',
+    'title': 'title',
+    'showUnit': 'unit_perform',
+    'masterUnit': 'unit_host',
+    'subUnit': 'unit_assist',
+    'supportUnit': 'unit_sponsor',
+    'otherUnit': 'unit_other',
+    'imageUrl': 'url_image',
+    'webSales': 'url_ticket',
+    'sourceWebPromote': 'url_flyer',
+    'editModifyDate': 'time_modified',
+    'startDate': 'time_start',
+    'endDate': 'time_end',
+}
