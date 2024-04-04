1c67bd4c142f3ee7566e8cea2e23b1f14dbbad9c
yellowsoar <yellowsoar@gmail.com>
2024-04-05 01:37:19 +0800

    add: category table generator function

--- src/opendata_tw/utils/update_schema.py
+++ src/opendata_tw/utils/update_schema.py
@@ -1,4 +1,5 @@
 import json
+import re
 from typing import (
     Any,
     Dict,
@@ -10,6 +11,7 @@ from typing import (
 )
 
 import requests
+from benedict import benedict
 
 
 def request_schema(
@@ -31,3 +33,52 @@ def request_schema(
     )
     data_json = json.loads(data_raw.text)
     return data_json
+
+
+def gen_category_table(
+    data_json: Dict,
+) -> Dict:
+    """Generate category table from dictionary
+
+    Parameters
+    ----------
+    data_json : Dict
+        The dictionary of all categories
+    """
+    data_schema = benedict(
+        data_json,
+        keypath_separator="..",
+    )
+    try:
+        category_text = str(
+            data_schema[
+                "..".join(
+                    [
+                        'components',
+                        'schemas',
+                        'Activity',
+                        'properties',
+                        'category',
+                        'description',
+                    ],
+                )
+            ],
+        )
+    except:
+        category_text = ""
+
+    if category_text:
+        category_text = re.sub(
+            r'活動類別 ([0-9]:.*)',
+            r'\1',
+            category_text,
+        )
+
+    if category_text:
+        category_array = category_text.split(' ')
+
+    category_table = {}
+    for single_category in category_array:
+        key_value = single_category.split(':')
+        category_table[key_value[0]] = key_value[1]
+    return category_table
