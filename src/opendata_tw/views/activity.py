492e24a621c667d84e119697548702b03608767d
yellowsoar <yellowsoar@gmail.com>
2024-04-05 02:44:56 +0800

    add: activity operation view

--- src/opendata_tw/views/activity.py
+++ src/opendata_tw/views/activity.py
@@ -1,1 +1,26 @@
+from django.shortcuts import (
+    redirect,
+    render,
+)
+from django_filters import rest_framework as drf_filters
+from opendata_tw import (
+    filters,
+    models,
+)
 
+
+def activity_operation(request):
+    filter_obj = filters.MusicalActivityFilter(
+        request.GET,
+        queryset=models.MusicalActivity.objects.all().order_by(
+            '-time_created',
+        ),
+    )
+
+    return render(
+        request,
+        'operation.html',
+        {
+            'filter': filter_obj,
+        },
+    )
