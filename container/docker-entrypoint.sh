9de164cff7fa988d7c893568aa7134a506f7ae1e
yellowsoar <yellowsoar@gmail.com>
2024-04-05 02:06:42 +0800

    chore: add docker-entrypoint.sh

---
+++ container/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+export C_FORCE_ROOT="true"
+
+python manage.py \
+	collectstatic \
+	--clear \
+	--link \
+	--noinput
+
+python manage.py \
+	migrate
+
+uvicorn \
+	musical_activity_viewer.asgi:application \
+	--host 0.0.0.0 \
+	--port 8000 \
+	--access-log &
+
