fe41cf58183fcfe9de3ed11eafe1083e0f31bf6b
yellowsoar <yellowsoar@gmail.com>
2024-04-07 09:09:30 +0800

    chore: activate daemons via supervisord

--- container/docker-entrypoint.sh
+++ container/docker-entrypoint.sh
@@ -4,7 +4,6 @@ a2enmod proxy proxy_http proxy_balancer lbmethod_byrequests
 
 a2ensite mav.conf
 
-apachectl start
 mkdir -p /var/log/supervisord
 
 python manage.py \
@@ -16,13 +15,6 @@ python manage.py \
 python manage.py \
 	migrate
 
-uvicorn \
-	musical_activity_viewer.asgi:application \
-	--host 0.0.0.0 \
-	--port 8000 \
-	--access-log &
 supervisord -n -c /tmp/supervisord.conf
 
-python manage.py \
-	qcluster
 supervisorctl start all
