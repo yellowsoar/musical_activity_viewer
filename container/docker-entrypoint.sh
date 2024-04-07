9a7915142245e09a7abaaeec7fc3b3b59927a277
yellowsoar <yellowsoar@gmail.com>
2024-04-07 09:05:58 +0800

    chore: install supervisord

--- container/docker-entrypoint.sh
+++ container/docker-entrypoint.sh
@@ -5,6 +5,7 @@ a2enmod proxy proxy_http proxy_balancer lbmethod_byrequests
 a2ensite mav.conf
 
 apachectl start
+mkdir -p /var/log/supervisord
 
 python manage.py \
 	collectstatic \
@@ -20,6 +21,8 @@ uvicorn \
 	--host 0.0.0.0 \
 	--port 8000 \
 	--access-log &
+supervisord -n -c /tmp/supervisord.conf
 
 python manage.py \
 	qcluster
+supervisorctl start all
